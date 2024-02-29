7c958d52841e7d5943f2c6c200ff17ff8158ca6a
sacha <sachadcarter@gmail.com>
2024-02-29 12:48:41 +0000

    moved error function out so it could be used for both password inputs
    fixed some css within the media query

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
 const passwordMsg = document.getElementById('password-msg');
 
-confirmPassword.addEventListener('change', () => {
-    if (password.value !== confirmPassword.value) {
+confirmPassword.addEventListener('change', toggleError);
+password.addEventListener('change', toggleError);
+
+function toggleError() {
+     if (password.value !== confirmPassword.value) {
         password.classList.add('error');
         confirmPassword.classList.add('error');
         passwordMsg.classList.add('error');
@@ -13,4 +16,4 @@ confirmPassword.addEventListener('change', () => {
         confirmPassword.classList.remove('error');
         passwordMsg.classList.remove('error');
     }
-})
+};
