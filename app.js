67e29c873660c6760cf3415f9ed6b63de1412cd7
sacha <sachadcarter@gmail.com>
2024-02-29 12:18:54 +0000

    added media query for screen size added js to validate passwords match

---
+++ app.js
@@ -0,0 +1,16 @@
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+const passwordMsg = document.getElementById('password-msg');
+
+confirmPassword.addEventListener('change', () => {
+    if (password.value !== confirmPassword.value) {
+        password.classList.add('error');
+        confirmPassword.classList.add('error');
+        passwordMsg.classList.add('error');
+    }
+    else {
+        password.classList.remove('error');
+        confirmPassword.classList.remove('error');
+        passwordMsg.classList.remove('error');
+    }
+})
